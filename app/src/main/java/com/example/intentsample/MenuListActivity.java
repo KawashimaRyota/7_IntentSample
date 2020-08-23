4d79aee63b61e97ad9ec2502c0b30dcdb68b9f18
kawashima.ryota <homerun0729@gmail.com>
2020-08-24 01:49:42 +0900

    Add MenuThanks Activity

--- app/src/main/java/com/example/intentsample/MenuListActivity.java
+++ app/src/main/java/com/example/intentsample/MenuListActivity.java
@@ -2,7 +2,10 @@ package com.example.intentsample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -80,5 +83,26 @@ public class MenuListActivity extends AppCompatActivity {
         SimpleAdapter adapter = new SimpleAdapter(MenuListActivity.this, menuList, android.R.layout.simple_list_item_2, from, to);
         //アダプタの登録
         lvMenu.setAdapter(adapter);
+        //リストタップのリスナクラス登録
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //タップされた行のデータを取得。SimpleAdapterでは1行分のデータはMap型
+            Map<String, String> item = (Map<String, String>) parent.getItemAtPosition(position);
+            //定食名と金額を取得
+            String menuName = item.get("name");
+            String menuPrice = item.get("price");
+            //インテントオブジェクトを生成
+            Intent intent = new Intent(MenuListActivity.this, MenuThanksActivity.class);
+            //第2画面に送るデータを格納
+            intent.putExtra("menuName", menuName);
+            intent.putExtra("menuPrice", menuPrice);
+            //第2画面の起動
+            startActivity(intent);
+        }
     }
 }
